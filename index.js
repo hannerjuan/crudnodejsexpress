a05220ae13383fa9ebcd09deacb9106cf11adaa0
Hanner Obando <hannerobando@outlook.com>
2024-11-27 16:55:00 -0500

    se adiciona CORS

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require("express");
 const constants=require("./lib/constants");
 const bodyParser = require('body-parser')
 const app = express();
+const cors = require('cors');
+
 
 // Importar rutas
 const encuestaRoutes = require('./routes/encuestaRoutes');
@@ -12,6 +14,15 @@ const paginaRoutes = require("./routes/paginaRoutes");
 const temaRoutes = require("./routes/temaRoutes");
 const muestraRoutes = require("./routes/muestraRoutes");
 const respuestaRoutes = require("./routes/respuestaRoutes");
+
+// // Use CORS for all routes
+// app.use(cors());
+
+app.use(cors({
+    origin: 'http://localhost:4200', // Cambia al puerto donde corre Angular
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 
 app.use(express.json());
@@ -30,11 +41,13 @@ app.use("/respuestas", respuestaRoutes);
 
 
 
+// app.use('/login', securityRouter);
 
-app.use('/login', securityRouter);
 
-
-app.use('/',securityRouter);
+// app.use('/',securityRouter);
 app.listen(constants.PORT, () =>{
     console.log(`El servidor se esta ejecutando en http://localhost:${constants.PORT}/`);
 });
+
+
+
